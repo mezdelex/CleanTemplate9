8871b2afeab6ffd7ed321d6b58da4b1744e6b24a
Mezdelex <alexcondegomez@gmail.com>
2024-08-02 21:59:50 +0200

    feat(getAllExpensesQuery,redis): added redisCache to getAllExpensesQuery

--- src/Application/Expenses/GetAllAsync/GetAllExpensesQueryHandler.cs
+++ src/Application/Expenses/GetAllAsync/GetAllExpensesQueryHandler.cs
@@ -1,5 +1,6 @@
 using Application.Contexts;
 using Application.Shared;
+using Domain.Cache;
 using MediatR;
 using static Domain.Extensions.Collections.Collections;
 
@@ -9,18 +10,37 @@ public sealed class GetAllExpensesQueryHandler
     : IRequestHandler<GetAllExpensesQuery, PagedList<ExpenseDTO>>
 {
     private readonly IApplicationDbContext _context;
+    private readonly IRedisCache _redisCache;
 
-    public GetAllExpensesQueryHandler(IApplicationDbContext context)
+    public GetAllExpensesQueryHandler(IApplicationDbContext context, IRedisCache redisCache)
     {
         _context = context;
+        _redisCache = redisCache;
     }
 
     public async Task<PagedList<ExpenseDTO>> Handle(
         GetAllExpensesQuery request,
         CancellationToken cancellationToken
-    ) =>
-        await _context
+    )
+    {
+        var redisKey = $"{nameof(GetAllExpensesQuery)}#{request.Page}#{request.PageSize}";
+        var cachedGetAllExpensesQuery = await _redisCache.GetCachedData<PagedList<ExpenseDTO>>(
+            redisKey
+        );
+        if (cachedGetAllExpensesQuery != null)
+            return cachedGetAllExpensesQuery;
+
+        var pagedExpenses = await _context
             .Expenses.OrderBy(e => e.Name)
             .Select(e => new ExpenseDTO(e.Id, e.Name, e.Description, e.Value, e.CategoryId))
             .ToPagedListAsync(request.Page, request.PageSize, cancellationToken);
+
+        await _redisCache.SetCachedData<PagedList<ExpenseDTO>>(
+            redisKey,
+            pagedExpenses,
+            DateTimeOffset.Now.AddMinutes(5)
+        );
+
+        return pagedExpenses;
+    }
 }
